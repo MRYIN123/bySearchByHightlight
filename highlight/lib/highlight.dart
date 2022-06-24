de1b7b2f2a411cdab222dfad64eb458b07f24a1b
yinsongbo <yinsongbo@leadigital.cn>
2022-06-24 15:22:31 +0800

    homepage

--- highlight/lib/highlight.dart
+++ highlight/lib/highlight.dart
@@ -4,4 +4,9 @@ library highlight;
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+
+//创建组件
+  void createHighlightSearchCompment() {
+    print('创建组件');
+  }
 }
